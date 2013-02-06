1400052c07e58d82d5fdaf0cee034c4b92a88bb7
Craig Hesling <craig@hesling.com>
2013-02-06 02:23:36 -0500

    Updated fib.c and fib-th.c to use the uintmax_t type as well.

--- fib-th.c
+++ fib-th.c
@@ -3,6 +3,9 @@
  */
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
+#include <inttypes.h>
+#include <limits.h>
 #include <assert.h>
 #include <pthread.h>
 
@@ -14,18 +17,21 @@
 	#define dprintf(format, ...) ((void)0)
 #endif
 
-long fib(int);
-long fib_base(long);
+/* uintmax_t has a max value of UINTMAX_MAX */
+typedef uintmax_t fibnum;
+
+fibnum fib(int);
+fibnum fib_base(fibnum);
 void *fib_th1(void *);
 void *fib_th2(void *);
 
 struct th{
 	pthread_t th;
-	long x;
-	long y;
+	fibnum x;
+	fibnum y;
 };
 
-long fib_base(long x)
+fibnum fib_base(fibnum x)
 {
 	if(x <= 2) return 1;
 	else return (fib_base(x-2) + fib_base(x-1));
@@ -34,27 +40,27 @@ long fib_base(long x)
 void *fib_th1(void *arg)
 {
 	struct th *th = (struct th *)arg;
-	dprintf("fib_th: th->x = %d, th->y = %d\n",th->x, th->y);
+	dprintf("fib_th: th->x = %"PRIuMAX", th->y = %"PRIuMAX"\n",th->x, th->y);
 	th->y = fib(th->x);
 	return ((void *) (0)) ;
 }
 void *fib_th2(void *arg)
 {
 	struct th *th = (struct th *)arg;
-	dprintf("fib_th: th->x = %d, th->y = %d\n",th->x, th->y);
+	dprintf("fib_th: th->x = %"PRIuMAX", th->y = %"PRIuMAX"\n",th->x, th->y);
 	th->y = fib_base(th->x);
 	return ((void *) (0)) ;
 }
 
-long fib (int x)
+fibnum fib (int x)
 {
 	dprintf("fib(%d): started\n",x);
 	if(x <= 2) return 1;
 	else
 	{
 		struct th th1, th2;
-		th1.x = (long) (x-2);
-		th2.x = (long) (x-1);
+		th1.x = (fibnum) (x-2);
+		th2.x = (fibnum) (x-1);
 		th1.y = th2.y = 0;
 
 		pthread_create(&th1.th, NULL, fib_th2, (void *)(&th1));
@@ -69,6 +75,6 @@ long fib (int x)
 int main(int argc, char *argv[]) {
 	int x = atoi(argv[1]);
 	printf("arg is: %d\n", x);
-	printf("fib(%d) = %ld\n",x, fib(x));
+	printf("fib(%d) = %"PRIuMAX"\n",x, fib(x));
 	return 0;
 }
--- fib.c
+++ fib.c
@@ -3,9 +3,15 @@
  */
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
+#include <inttypes.h>
+#include <limits.h>
 #include <assert.h>
 
-long fib (int x)
+/* uintmax_t has a max value of UINTMAX_MAX */
+typedef uintmax_t fibnum;
+
+fibnum fib (int x)
 {
 	if(x <= 2) return 1;
 	else return (fib(x-2) + fib(x-1));
@@ -14,6 +20,6 @@ long fib (int x)
 int main(int argc, char *argv[]) {
 	int x = atoi(argv[1]);
 	printf("arg is: %d\n", x);
-	printf("fib(%d) = %ld\n",x,fib(x));
+	printf("fib(%d) = %"PRIuMAX"\n",x,fib(x));
 	return 0;
 }
